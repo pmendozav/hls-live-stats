16b942369919086069b9b2663cb4b2d73331c5ca
Pavel <pavel@haystack.tv>
2022-07-13 12:01:38 -0500

    dummy logic to detect inband/outband CC

--- helper/source.js
+++ helper/source.js
@@ -2,6 +2,9 @@ const axios = require("axios");
 const path = require('path');
 const { parse } = require('url');
 var m3u8Parser = require('m3u8-parser');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const fs = require('fs');
 
 const { simpleParser } = require('./helper');
 
@@ -32,11 +35,58 @@ class Source {
                 axios.get(variantUrl)
                 .then(resp => {
                     this.errorsCount = 0;
-                    const { info } = simpleParser(resp.data, '', '');
-                    this.stats.adMarkers += info.adMarkers;
-                    this.stats.adsDuration += info.adsDuration;
-                    this.stats.contentDuration += info.contentDuration;
-                    setTimeout(lambda, info.contentDuration * 1000);
+
+                    const _ = () => {
+                        const { info } = simpleParser(resp.data, '', '');
+                        this.stats.adMarkers += info.adMarkers;
+                        this.stats.adsDuration += info.adsDuration;
+                        this.stats.contentDuration += info.contentDuration;
+                        setTimeout(lambda, info.contentDuration * 1000);
+                    }
+
+                    const parser = new m3u8Parser.Parser();
+                    parser.push(resp.data);
+                    parser.end();
+
+                    if (this.hasCC()) return _();
+
+                    // try to retrieve the first segment from the playlist manifest if this.stats.ccInBand is false or is not defined
+                    let regex = /^(https:\/\/|http:\/\/)(.*)?$/;
+                    let firstSegment = '';
+                    const index = parser.manifest.segments.length - 1;
+                    if (index >= 0 && parser.manifest.segments && parser.manifest.segments[index] && parser.manifest.segments[index].uri) {
+                        firstSegment = parser.manifest.segments[index].uri;
+                    }
+
+                    let uri = '';
+                    if (regex.exec(firstSegment)) {
+                        uri = firstSegment;
+                    } else {
+                        const url = new URL(variantUrl);
+                        const basePath = path.dirname(url.hostname + url.pathname);
+                        uri = `https://${path.join(basePath, firstSegment)}`
+                    }
+
+                    if (!regex.exec(uri)) {
+                        console.error(`WRONG URI=${uri} (used to retrieve CC)`);
+                        return _();
+                    }
+
+                    // try to read CC from the first segment
+                    axios.get(uri, {
+                        responseType: 'arraybuffer'
+                    })
+                    .then(async response => {
+                        const fname = '/tmp/test.ts';
+                        fs.writeFileSync(fname, Buffer.from(response.data));
+                        const result = await exec(`ccextractor -stdout ${fname}`)
+                            .then((result) => result.stdout)
+                            .catch(err => err.message);
+
+                        // dummy way to detect CC
+                        this.stats.ccInBand = result.indexOf('-->') > -1;
+                        _();
+                    })
                 })
             })
             .catch(() => {
@@ -56,6 +106,7 @@ class Source {
         }
 
         if (rawData) {
+            if (!this.hasCC()) this.stats.ccOutBand = this.checkExternalCC(rawData);
             const variantUrl = this.getVariantUrl(rawData);
             axios.get(variantUrl)
                 .then(resp => {
@@ -70,6 +121,14 @@ class Source {
         } else {
             setTimeout(lambda, defaultTimer * 1000)
         }
+    }
+
+    checkExternalCC(data) {
+        return data.split('\n').findIndex(s => s.indexOf('#EXT-X-MEDIA') >= 0 && s.indexOf('.m3u8') >= 0) >= 0;
+    }
+
+    hasCC() {
+        return this.stats.ccInBand || this.stats.ccOutBand
     }
 
     getVariantUrl(rawData) {
